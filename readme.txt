69b390d68e1d3cf59955d516b32accdecedce3f9
Akeda Bagus <akeda@x-team.com>
2014-04-14 14:07:25 +0700

    Init.

---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Slack Gravity Forms ===
+Contributors:      akeda
+Donate link:       http://goo.gl/DELyuR
+Tags:              slack, api, chat, notification, gf, gravity, forms
+Requires at least: 3.6
+Tested up to:      3.8.1
+Stable tag:        trunk
+License:           GPLv2 or later
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+Send notifications to Slack channels whenever new submission entries, for Gravity Forms, are received.
+
+== Description ==
+
+This plugin is an extension to [Slack plugin](http://wordpress.org/plugins/slack) that allows you to send notifications to Slack channels whenever new submission entries, for Gravity Forms, are received.
+
+The new event will be shown on integration setting with text **When new submission entry, for Gravity Forms, received**. If checked then notification will be delivered once submission entries are received.
+
+You can alter the message with `slack_gform_after_submission_message` filter. The filter receives following parameters (ordered by position):
+
+* `$entry` &mdash; The entry that was just created
+* `$form` &mdash; The current form
+
+See [Gravity Forms documentation](http://www.gravityhelp.com/documentation/page/Gform_after_submission).
+
+**Development of this plugin is done on [GitHub](https://github.com/gedex/wp-slack-edd). Pull requests are always welcome**.
+
+== Installation ==
+
+1. You need to install and activate both Gravity Forms and [Slack](http://wordpress.org/plugins/slack) plugins first.
+1. Then upload **Slack Gravity Forms** plugin to your blog's `wp-content/plugins/` directory and activate.
+1. You will see new event type with text **When new submission entry, for Gravity Forms, received** in integration setting. If checked then notification will be delivered once submission entries are received.
+
+== Screenshots ==
+
+1. Event option in integration setting
+1. Your channel get notified whenever new submission entries are received.
+
+== Changelog ==
+
+= 0.1.0 =
+Initial release
---
+++ slack-gravityforms.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Plugin Name: Slack Gravity Forms
+ * Plugin URI: http://gedex.web.id/wp-slack-gravityforms/
+ * Description: This plugin allows you to send notifications to Slack channels whenever new submission entries, for Gravity Forms, are received.
+ * Version: 0.1.0
+ * Author: Akeda Bagus
+ * Author URI: http://gedex.web.id
+ * Text Domain: slack
+ * Domain Path: /languages
+ * License: GPL v2 or later
+ * Requires at least: 3.6
+ * Tested up to: 3.8
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+function wp_slack_gform_after_submission( $events ) {
+	$events['gform_after_submission'] = array(
+		// Action in Gravity Forms to hook in to get the message.
+		'action' => 'gform_after_submission',
+
+		// Description appears in integration setting.
+		'description' => __( 'When new submission entry, for Gravity Forms, received', 'slack' ),
+
+		// Message to deliver to channel. Returns false will prevent
+		// notification delivery.
+		'message' => function( $entry, $form ) {
+
+			$admin_url = add_query_arg(
+				array(
+					'page' => 'gf_entries',
+					'view' => 'entry',
+					'id'   => $entry['form_id'],
+					'lid'  => $entry['id'],
+				),
+				admin_url( 'admin.php' )
+			);
+
+			return apply_filters( 'slack_gform_after_submission_message',
+				sprintf(
+					__( 'New submission for *<%s|%s>* form on *%s*. *<%s|See entry>*.', 'slack' ),
+
+					$entry['source_url'],
+					$form['title'],
+					$entry['date_created'],
+					$admin_url
+				),
+
+				$entry,
+				$form
+			);
+		}
+	);
+
+	return $events;
+}
+add_action( 'slack_get_events', 'wp_slack_gform_after_submission' );
